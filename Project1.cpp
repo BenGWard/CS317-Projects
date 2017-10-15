6571cbc3832f2f7b228260b90b706b77f9c33bdc
Ben Ward <ben.g.ward@gmail.com>
2017-10-15 16:26:02 -0500

    Adding project files

---
+++ Project1.cpp
@@ -0,0 +1,94 @@
+//	Class:		CS317
+//	Name:		Ben Ward
+//	Email:		bward1@my.athens.edu
+//	Student ID:	00076160
+//	Title:		Project 1
+
+#include <iostream>	//for cin & cout
+#include <iomanip>	//for setw
+#include <cmath>	//for pow function
+
+using namespace std;
+
+int main()
+{	
+	//constant variables
+	const int FIRSTVOLTAGE = 20;
+	const int SECONDVOLTAGE = 35;
+	const int THIRDVOLTAGE = 15;
+	const int FOURTHVOLTAGE = 6;
+	const int FIRSTRESISTANCE = 10;
+	const int SECONDRESISTANCE = 1000;
+	const float FIRSTTIMECONSTANT = 0.16;
+	const float SECONDTIMECONSTANT = 0.55;
+	const float THIRDTIMECONSTANT = 2.6;
+	const float FOURTHTIMECONSTANT = 0.044;
+	const float EULERSNUMBER = 2.71828;
+	
+	//variables to store time data
+	float firstTime;
+	float secondTime;
+	float thirdTime;
+	float fourthTime;
+	float fifthTime;
+	float sixthTime;
+	float seventhTime;
+	float eighthTime;
+	float ninthTime;
+	
+	//prompt to get time values from user
+	cout << endl << "Enter the nine times seperated by a space: " << endl;
+	cin >> firstTime >> secondTime >> thirdTime >> fourthTime >> fifthTime;
+	cin >> sixthTime >> seventhTime >> eighthTime >> ninthTime;
+	
+	//this section declares current variables and does calculations
+	float firstCurrent = (static_cast<float>(FIRSTVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-firstTime/FOURTHTIMECONSTANT));
+	float secondCurrent = (static_cast<float>(SECONDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-secondTime/FIRSTTIMECONSTANT));
+	float thirdCurrent = (static_cast<float>(SECONDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-thirdTime/FIRSTTIMECONSTANT));
+	float fourthCurrent = (static_cast<float>(SECONDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-fourthTime/FIRSTTIMECONSTANT));
+	float fifthCurrent = (static_cast<float>(SECONDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-fifthTime/FIRSTTIMECONSTANT));
+	float sixthCurrent = (static_cast<float>(THIRDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-sixthTime/SECONDTIMECONSTANT));
+	float seventhCurrent = (static_cast<float>(THIRDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-seventhTime/SECONDTIMECONSTANT));
+	float eighthCurrent = (static_cast<float>(THIRDVOLTAGE)/FIRSTRESISTANCE) * pow(EULERSNUMBER, (-eighthTime/SECONDTIMECONSTANT));
+	float ninthCurrent = (static_cast<float>(FOURTHVOLTAGE)/SECONDRESISTANCE) * pow(EULERSNUMBER, (-ninthTime/FOURTHTIMECONSTANT));
+	
+	//this section is the output of the table of data
+	//I have wrapped around my cout statements to make them visable on one page
+	cout << setprecision(3) << fixed;
+	cout << endl << endl;
+	
+	cout << "Voltage V" << setw(15) << "Resistance R" << setw(11) << "RC";
+	cout << setw(21) << "Time t" << setw(15) << "Current i" << endl;
+	
+	cout << "(Volts)" << setw(13) << "(Ohms)" << setw(22) << "(Time Constant)";
+	cout << setw(15) << "(Seconds)" << setw(13) << "(Amps)" << endl;
+	
+	cout << setw(4) << FIRSTVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << FOURTHTIMECONSTANT << setw(19) << firstTime << setw(15) << firstCurrent << endl; 
+	
+	cout << setw(4) << SECONDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << FIRSTTIMECONSTANT << setw(19) << secondTime << setw(15) << secondCurrent << endl;
+	
+	cout << setw(4) << SECONDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << FIRSTTIMECONSTANT << setw(19) << thirdTime << setw(15) << thirdCurrent << endl;
+	
+	cout << setw(4) << SECONDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << FIRSTTIMECONSTANT << setw(19) << fourthTime << setw(15) << fourthCurrent << endl;
+	
+	cout << setw(4) << SECONDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << FIRSTTIMECONSTANT << setw(19) << fifthTime << setw(15) << fifthCurrent << endl;
+	
+	cout << setw(4) << THIRDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << SECONDTIMECONSTANT << setw(19) << sixthTime << setw(15) << sixthCurrent << endl;
+	
+	cout << setw(4) << THIRDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << SECONDTIMECONSTANT << setw(19) << seventhTime << setw(15) << seventhCurrent << endl;
+	
+	cout << setw(4) << THIRDVOLTAGE << setw(14) << FIRSTRESISTANCE << setw(18);
+	cout << SECONDTIMECONSTANT << setw(19) << eighthTime << setw(15) << eighthCurrent << endl;
+	
+	cout << setw(4) << FOURTHVOLTAGE << setw(14) << SECONDRESISTANCE << setw(18);
+	cout << THIRDTIMECONSTANT << setw(19) << ninthTime << setw(15) << ninthCurrent << endl;
+		
+	return (0);
+}
---
+++ Project2.cpp
@@ -0,0 +1,134 @@
+//	Class:		CS317
+//	Name:		Ben Ward
+//	Email:		bward1@my.athens.edu
+//	Student ID:	00076160
+//	Title:		Project 2
+
+#include <iostream>	
+#include <iomanip> //needed for setprecision
+
+using namespace std;
+
+int main()
+{	
+	//constant variables
+	int REGBASECOST = 6;
+	int REGFREEMIN = 50;
+	float REGMINCOST = 0.20;
+	int PREBASECOST = 15;
+	int PREDAYFREEMIN = 75;
+	float PREDAYMINCOST = 0.10;
+	int PRENIGHTFREEMIN = 100;
+	float PRENIGHTMINCOST = 0.05;
+	
+	//variables
+	int accountNumber;
+	char serviceCode;
+	int minutesUsedDay;
+	int minutesUsedNight;
+	float amountDue = 0.0;    //just to be sure we start at 0
+	
+	cout << endl << "\t\t\tWardPhone" << endl << endl << endl;
+	cout << "Please enter account number: ";
+	cin >> accountNumber;
+	if (accountNumber < 0)
+			{
+				cout << "You have entered a negative number. Please run program again." << endl;
+				return 0;
+			}
+	cout << "Please enter service type, premium or regular (P/R): ";
+	cin >> serviceCode;
+	
+	switch (serviceCode)
+	{
+		case 'P':
+		case 'p':
+			cout << endl << "Enter number of minutes used 6:00am to 6:00pm: ";
+			cin >> minutesUsedDay;
+			cout << "Enter number of minutes used 6:00pm to 6:00am: ";
+			cin >> minutesUsedNight;
+	
+			//validates that the minutes input are positive
+			if (minutesUsedDay < 0 || minutesUsedNight < 0)
+			{
+				cout << "You have entered a negative number. Please run program again." << endl;
+				return 0;
+			}
+			
+			//this section checks to see if they have used more than
+			//the free included minutes and calculates the bill
+			if (minutesUsedDay > PREDAYFREEMIN)
+			{
+				amountDue = PREBASECOST + (minutesUsedDay - PREDAYFREEMIN) * PREDAYMINCOST;
+			}
+		
+			else
+			{
+				amountDue = PREBASECOST;
+			}
+		
+			if (minutesUsedNight > PRENIGHTFREEMIN)
+			{
+				amountDue = amountDue + (minutesUsedNight - PRENIGHTFREEMIN) * PRENIGHTMINCOST;
+			}
+		
+			//output to user
+			cout << endl << endl << endl;
+			cout << "*******************************************************" << endl;
+			cout << "Account Number:" << accountNumber << endl << endl;
+			cout << "Premium Service" << endl << endl;
+			cout << "Minutes used 6:00am to 6:00pm: " << minutesUsedDay << endl;
+			cout << "Minutes used 6:00pm to 6:00am: " << minutesUsedNight << endl;
+			
+			//set up output for money
+			cout << setprecision(2) << fixed;
+			cout << endl << "____________________" << endl;
+			cout << "Amount Due: $" << amountDue << endl;
+		
+			break;
+		
+		case 'R':
+		case 'r':
+			cout << endl << "Enter number of minutes used: ";
+			cin >> minutesUsedDay;
+			
+			//validates that the minutes input are positive
+			if (minutesUsedDay < 0)
+			{
+				cout << "You have entered a negative number. Please run program again." << endl;
+				return 0;
+			}
+			
+			//this section checks to see if they have used more than
+			//the free included minutes and calculates the bill
+			if (minutesUsedDay > REGFREEMIN)
+			{
+				amountDue = REGBASECOST + (minutesUsedDay - REGFREEMIN) * REGMINCOST;
+			}
+		
+			else
+			{
+				amountDue = REGBASECOST;
+			}
+		
+			//output to user
+			cout << endl << endl << endl;
+			cout << "*******************************************************" << endl;
+			cout << "Account Number:" << accountNumber << endl << endl;
+			cout << "Regular Service" << endl << endl;
+			cout << "Minutes used: " << minutesUsedDay << endl;
+			
+			//set up output for money
+			cout << setprecision(2) << fixed; 
+			cout << endl << "____________________" << endl;
+			cout << "Amount Due: $" << amountDue << endl;
+		
+			break;
+		
+		default:
+			cout << "You have entered an invalid input. ";
+			cout << "Only P, p, R, or r accepted. Please run program again." << endl;
+	}
+	
+	return 0;
+}
