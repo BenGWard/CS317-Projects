f2e399184cba715aee65470590ea40e1b34078b3
Ben Ward <ben.g.ward@gmail.com>
2017-10-28 15:25:58 -0500

    Initial commit for Project4

---
+++ Project4.cpp
@@ -0,0 +1,119 @@
+//	Class:		CS317
+//	Name:		Ben Ward
+//	Title:		Project 4
+
+#include <iostream>
+#include <iomanip> //needed for setprecision and setw
+
+using namespace std;
+
+int main()
+{	
+	return 0;
+}
+
+
+// ********************************************************
+// The daysSpent function asks for the number of days     *
+// spent on the trip and returns the value as an integer. *
+// ********************************************************
+
+
+// ********************************************************
+// The times function asks for the time of departure and  *
+// the time of return. The times are validated and stored *
+// in the reference parameter variables: start and end.   *
+// ********************************************************
+
+
+// ********************************************************
+// The isValidTime function accepts a double argument     *
+// assumed to be a time in the 24 hour format of HH.MM.   *
+// The value is tested, and if it is valid the function   *
+// returns true. Otherwise it returns false.              *
+// ********************************************************
+
+// ********************************************************
+// The airFare function asks the user to enter the amount *
+// of any airfare the employee incurred, adds the value   *
+// to the totalExpenses, and returns the airfare amount   *
+// as a double.                                           *
+// ********************************************************
+
+
+// ********************************************************
+// The carRental function asks the user to enter the      *
+// amount of car rental fees the employee incurred, adds  *
+// the value to the totalExpenses, and returns the amount *
+// of car rental fees as a double.                        *
+// ********************************************************
+
+
+// ********************************************************
+// The vehicle function asks the user to enter the number *
+// of miles a private vehicle was driven.The expenses are *
+// added to the totalExpenses global variable. Allowable  *
+// expenses are returned as a double.                     *
+// ********************************************************
+
+// ********************************************************
+// The parking function asks the user to enter the amount *
+// of any parking fees. The expenses are added to the     *
+// totalExpenses global variable. Allowable expenses are  *
+// returned as a double.                                  *
+// ********************************************************
+
+
+// ********************************************************
+// The taxi function asks the user to enter the amount    *
+// of any taxi fees. The expenses are added to the        *
+// totalExpenses global variable. Allowable expenses are  *
+// returned as a double.                                  *
+// ********************************************************
+
+
+// ********************************************************
+// The registration function asks the user to enter the   *
+// amount of conference registration fees incurred by the *
+// employee, adds the amount to totalExpenses, and        *
+// returns the amount spent as a double.                  *
+// ********************************************************
+
+
+// ********************************************************
+// The hotel function asks the user to enter the amount   *
+// of hotel expenses. The expenses are added to the       *
+// totalExpenses global variable. Allowable expenses are  * 
+// returned as a double.                                  *
+// ********************************************************
+
+// ********************************************************
+// The meals function calls the getBreakfast, getLunch,   *
+// and getDinner functions to calculate the employee's    *
+// meal expenses. The expenses are added to the           *
+// totalExpenses global variable. Allowable expenses are  *
+// returned as a double.                                  *
+// ********************************************************
+
+
+// ********************************************************
+// The getBreakfast function asks the user for the amount *
+// spent on a breakfast meal. The expenses are added to   *
+// the totalExpenses global variable. Allowable expenses  *
+// are returned as a double.                              *
+// ********************************************************
+
+
+// ********************************************************
+// The getLunch function asks the user for the amount     *
+// spent on a lunch meal. The expenses are added to the   *
+// totalExpenses global variable. Allowable expenses are  *
+// returned as a double.                                  *
+// ********************************************************
+
+// ********************************************************
+// The getDinner function asks the user for the amount    *
+// spent on a dinner meal. The expenses are added to the  *
+// totalExpenses global variable. Allowable expenses are  *
+// returned as a double.                                  *
+// ********************************************************
